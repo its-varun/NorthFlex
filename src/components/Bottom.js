a65f5d497267ca0f90cdec67a69e62172e9a4db4
Varun Kansal <varunkansal2111@gmail.com>
2022-04-21 20:17:37 +0530

    Home page updated

---
+++ src/components/Bottom.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import './Bottom.css'
+import footer from "../WebsiteMaterial/images/footer.png"
+
+export default function Bottom() {
+  return (
+    <>
+        <div id="footer">
+            <div id="first_footer">
+                <h1 id="help">We're here to help you</h1>
+                <a href="#">
+                    <button className="button">
+                        Contact Us
+                    </button>
+                </a>
+                <a href="#">
+                    <button className='button' style={{marginLeft:"20px"}}>
+                        Dealership  Inquiry
+                    </button>
+                </a>
+                <h3 className='info' style={{marginTop:"35px"}}>
+                    1234-567-9000
+                </h3>
+                <h3 className='info'>
+                    northflexxxxx@gmail.com
+                </h3>
+                <h3 className='info'>
+                    https://nothflex.in/
+                </h3>
+            </div>
+            <div id="second_footer">
+                <h1 id="main">NorthFlex</h1>
+                <h3 className='info'>
+                    Aluminium Cables
+                </h3>
+                <h3 className='info'>
+                    CCTV Cables
+                </h3>
+                <h3 className='info'>
+                    Co-Axial Cables
+                </h3>
+                <h3 className='info'>
+                    Flexible Wires
+                </h3>
+                <h3 className='info'>
+                    Multistrand Wires
+                </h3>
+                <div id="contact_icons">
+                    <a href="https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwjlgeDorbzwAhVPDCsKHVF8BJIYABAAGgJzZg&ae=2&ohost=www.google.com&cid=CAESQOD2jimxu7s00ezwY1HM_qBD0C7yH-6Ezk32Mn55FpiS8HLHplRfnSTcs-DkW3zfUdKXhPzSWT5JIjTh7TRHTPY&sig=AOD64_2V5kLYi7mDRCGZIpV5Ww7Q2ntx7Q&q&adurl&ved=2ahUKEwidmtjorbzwAhW0heYKHZaRCQEQ0Qx6BAgGEAE" class="link_footer"><i  id="contact_icon" class="fab fa-facebook-f"></i></a>
+                    <a href="https://www.instagram.com/" class="link_footer"><i  id="contact_icon" class="fab fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/" class="link_footer"><i  id="contact_icon" class="fab fa-linkedin"></i></a>
+                    <a href="https://www.apple.com/in/" class="link_footer"><i  id="contact_icon" class="fab fa-apple"></i></a>
+                    <a href="https://twitter.com/?lang=en" class="link_footer"><i  id="contact_icon" class="fab fa-twitter"></i></a>
+                </div>
+            </div>
+
+            <div id="third_footer">
+                <img src={footer} id="footer_image" />
+            
+            </div>
+
+        </div>    
+    
+    </>
+  )
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,11 @@ import Northflex_logo from "../WebsiteMaterial/images/Northflex_logo.jpg"
 import img2 from "../WebsiteMaterial/images/img2.jpg"
 import img3 from "../WebsiteMaterial/images/img3.jpg"
 import img4 from "../WebsiteMaterial/images/img4.jpg"
-
+import fire from "../WebsiteMaterial/images/fire.jpg"
+import electrician from "../WebsiteMaterial/images/electrician.jpg"
+import longLasting from "../WebsiteMaterial/images/longLasting.png"
+import meter from "../WebsiteMaterial/images/meter.jpg"
+import Bottom from "./Bottom";
 
 
 
@@ -32,6 +36,23 @@ export default function Home() {
 	<div id="main_photo">
 		<input type="text" id="search_main" placeholder="Search " />
 	</div>
+
+	<div id="intro" >
+		<h2 id="welcome">
+			WELCOME TO NORTHFLEX
+		</h2>
+		<hr id="line_northflex" />
+
+		<div id="intro_mid">
+			<img src={electrician} id="electrician" />
+			<p>
+				
+We introduce ourselves as leading manufacturer of electrical goods under brand name of NIROL. Garg Industries established in the year 2000, is a professional company having specialized in designing, manufacturing and marketing of electrical wires, cables, main switch gear, change over switches, Kit-Kat fuse, domestic and similar purpose switches and socket outlets and heating elements. Our mission is to go to extra mile in satisfying our customers has been instrumental in propelling the company to a premiership position in the niche market of QUALITY WIRES, CABLES & ELECTRIC ASSESSORIES for the domestic and industrial use.   Quality is the life of enterprise! Garg Industries always provide first class products to its sufficient customers and provide excellent service to co-operative clients with honestly and eco friendly nature.
+			</p>
+
+		</div>
+	</div>
+
 	<div id="wrapper">
 		<div style={{display:"flex"}}>
 			<h1 id="wrapper_heading">Our Products</h1>
@@ -63,6 +84,7 @@ export default function Home() {
 			</div> 
 		</div>
 	</div>
+
 	<div id="elevate">
 		<div id="elevate_box">
 			<p>  ELEVATE YOUR HOME<br/>
@@ -73,6 +95,43 @@ export default function Home() {
 		</div>
 		
 	</div>
+
+	<h1 id="quality">
+		Quality . Guarantted
+	</h1>
+	<div id="advantages">
+		<div className='cards'>
+			<div className="imgBx">
+				<img src={fire} id="img"/>
+			</div>
+			<div className="contents">
+				<h2>fire</h2>
+				<p>dsffffffffffffffffffff</p>
+			</div>
+		</div>
+
+		<div className='cards'>
+			<div className="imgBx">
+				<img src={longLasting} id="img"/>
+			</div>
+			<div className="contents">
+				<h2>long life</h2>
+				<p>ewfweffwefwfefewfwewffe</p>
+			</div>
+		</div>
+
+		<div className='cards'>
+			<div className="imgBx">
+				<img src={meter} id="img"/>
+			</div>
+			<div className="contents">
+				<h2>meter</h2>
+				<p>ejariewihkdefewuohfeneiods</p>
+			</div>
+		</div>
+	</div>
+
+	<Bottom/>
     </>
   )
 }
